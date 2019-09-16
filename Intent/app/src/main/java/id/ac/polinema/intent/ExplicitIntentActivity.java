5e466661a760a01bd5cd26a8ee452337a7451e74
yogaibnu <yogaibnudinataa@gmail.com>
2019-09-16 13:54:32 +0700

    explicit intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    private EditText nameInput;
+    private TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
     }
 
+    public void onSubmit(View view) {
+        String name = nameInput.getText().toString();
+        outputText.setText("Hello " + name);
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -2,14 +2,28 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
+    public void handleExplicitIntent(View view) {
+        Intent intent =  new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void handleExit(View view) {
+        finish();
+    }
 }
